e1a5b6e83f931ec0299435e3485cd0149aee1f05
marit.van.dijk <mlvandijk@gmail.com>
2023-11-22 08:29:40 +0100

    Add Logging

--- src/main/java/com/maritvandijk/orderservice/OrderService.java
+++ src/main/java/com/maritvandijk/orderservice/OrderService.java
@@ -1,6 +1,8 @@
 package com.maritvandijk.orderservice;
 
 import com.maritvandijk.orderservice.model.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -11,6 +13,8 @@ class OrderService {
 
     final
     OrderRepository orderRepository;
+
+    private static final Log log = LogFactory.getLog(OrderService.class);
 
     public OrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
@@ -25,6 +29,7 @@ class OrderService {
     }
 
     public void registerOrder(CustomerOrder order) {
+        log.info("Registering order " + order.getOrderId());
         CustomerOrder updatedOrder = checkAndUpdateOrder(order);
         orderRepository.save(updatedOrder);
     }
@@ -37,6 +42,7 @@ class OrderService {
 
     private static CustomerOrder cancelOrderItemsIfPaymentFailed(CustomerOrder order) {
         if (order.getPaymentInformation().getPaymentStatus() == PaymentStatus.FAILED) {
+            log.info("Payment failed for order" + order.getOrderId() + ". Cancelling order items.");
             order.getOrderItems().stream().filter(item -> item.getCancellations().isEmpty()).forEach(item -> {
                 Cancellation cancellation = new Cancellation(CancellationReason.PAYMENT_FAILED, item.getNumberOfItems());
                 item.setCancellations(Collections.singletonList(cancellation));
@@ -46,8 +52,11 @@ class OrderService {
     }
 
     private CustomerOrder clearInvalidPhoneNumber(CustomerOrder order) {
+        // Check that the number we have is a valid number
+        // If the number is not valid, remove the invalid number
         boolean isValidPhoneNumber = isValidPhoneNumber(order.getCustomer().getBillingAddress().getPhoneNumber());
         if (!isValidPhoneNumber) {
+            log.info("Removing invalid phone number" + order.getCustomer().getBillingAddress().getPhoneNumber());
             Address billingAddress = order.getCustomer().getBillingAddress();
             billingAddress.setPhoneNumber(null);
         }
